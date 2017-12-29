dd37863a54d23fd46077d8442ad16cbbfd0e5bb8
Mary Simmons <MarySimmons@Marys-MacBook-Pro.local>
2017-12-29 15:56:33 -0700

    created a js for loop to run thru all the questions to insure that each question alerts if you dont make a selection for them.

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -1,5 +1,5 @@
 function submitAnswers(){
-  var total = 5;
+  var total = 8;
   var score = 0;
 
 //obtaining user input
@@ -8,12 +8,27 @@ function submitAnswers(){
   var q3 = document.forms["quizform"]["q3"].value;
   var q4 = document.forms["quizform"]["q4"].value;
   var q5 = document.forms["quizform"]["q5"].value;
-
+  var q6 = document.forms["quizform"]["q6"].value;
+  var q7 = document.forms["quizform"]["q7"].value;
+  var q8 = document.forms["quizform"]["q8"].value;
 
 
 // validation
-  if (q1 == null || q1 == ""){
-      alert("you missed q1");
-      return false;
+
+// this is the for loop that will run the function that says you can't miss selecting any of the questions. Below however, only applies to one question but we want it to run thru all eight, so in order to change q1 the q2, q3, ect, we need to put an eval on it. See below
+
+    // for (i = 0; i <= total; i++) {
+    //    if (q1 == null || q1 == ""){
+    //        alert("you missed q1");
+    //        return false;
+    //      }
+    for(i = 1; i <= total; i++){
+       if (eval("q"+ i) == null || eval("q" + i) == ""){
+           alert("you missed question " + i);
+           return false;
+         }
+
+
     }
+
 }
