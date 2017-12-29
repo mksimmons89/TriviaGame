f11756693fcc4b6f7722f6ec63656409db734883
Mary Simmons <MarySimmons@Marys-MacBook-Pro.local>
2017-12-29 14:48:40 -0700

    got my game to recognizwhen an answer wasn't being filled out for the first question

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -3,12 +3,17 @@ function submitAnswers(){
   var score = 0;
 
 //obtaining user input
-var q1 = document.forms["quizform"]["q1"].value;
-var q2 = document.forms["quizform"]["q2"].value;
-var q3 = document.forms["quizform"]["q3"].value;
-var q4 = document.forms["quizform"]["q4"].value;
-var q5 = document.forms["quizform"]["q5"].value;
+  var q1 = document.forms["quizform"]["q1"].value;
+  var q2 = document.forms["quizform"]["q2"].value;
+  var q3 = document.forms["quizform"]["q3"].value;
+  var q4 = document.forms["quizform"]["q4"].value;
+  var q5 = document.forms["quizform"]["q5"].value;
 
 
-alert(q1);
+
+// validation
+  if (q1 == null || q1 == ""){
+      alert("you missed q1");
+      return false;
+    }
 }
