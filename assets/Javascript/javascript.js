ec980381e9f5a379f683aacd7a6d36d7a576ddc9
Mary Simmons <MarySimmons@Marys-MacBook-Pro.local>
2017-12-28 22:00:03 -0700

    hooked up my javascriptand got my html to recognize when a choice was being answered

---
+++ assets/Javascript/javascript.js
@@ -0,0 +1,14 @@
+function submitAnswers(){
+  var total = 5;
+  var score = 0;
+
+//obtaining user input
+var q1 = document.forms["quizform"]["q1"].value;
+var q2 = document.forms["quizform"]["q2"].value;
+var q3 = document.forms["quizform"]["q3"].value;
+var q4 = document.forms["quizform"]["q4"].value;
+var q5 = document.forms["quizform"]["q5"].value;
+
+
+alert(q1);
+}
